ef7d8c67edef08200f0fc87f430137ac9f86db98
Bartek Kotkowski <bartek.kotkowski@cgg.com>
2021-07-20 13:32:54 -0500

    Added sample pybing11/Qt application

---
+++ sample/http/http_py11.cpp
@@ -0,0 +1,10 @@
+#include <pybind11/pybind11.h>
+
+int openDownloadApp();
+
+namespace py = pybind11;
+
+PYBIND11_MODULE(http_py, m) {
+    m.doc() = "pybind11 wrapper for openssl test";
+    m.def("OpenDownloadApp", &openDownloadApp, "Open Download App");
+}
