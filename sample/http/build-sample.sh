66b913e84244c1db00ec14e4d6de99a7f2f4f073
Bartek Kotkowski <bartek.kotkowski@cgg.com>
2021-07-22 09:04:39 -0500

    Fixed sample code build script

--- sample/http/build-sample.sh
+++ sample/http/build-sample.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-/vcpkg/vcpkg/downloads/tools/cmake-3.20.2-linux/cmake-3.20.2-linux-x86_64/bin/cmake -B build -S . -DCMAKE_TOOLCHAIN_FILE=/vcpkg/vcpkg/scripts/buildsystems/vcpkg.cmake -DVCPKG_TARGET_TRIPLET=x64-linux-dynamic -DVCPKG_OVERLAY_TRIPLETS=custom-triplets
+/vcpkg/vcpkg/downloads/tools/cmake-3.20.2-linux/cmake-3.20.2-linux-x86_64/bin/cmake -B build -S . -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=/vcpkg/vcpkg/scripts/buildsystems/vcpkg.cmake -DVCPKG_TARGET_TRIPLET=x64-linux-dynamic
 /vcpkg/vcpkg/downloads/tools/cmake-3.20.2-linux/cmake-3.20.2-linux-x86_64/bin/cmake --build build -DCMAKE_BUILD_TYPE=Release
