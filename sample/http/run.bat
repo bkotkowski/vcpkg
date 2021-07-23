b95201f6cc3c7cece1cc232c4a1099718d302a20
Kotkowski <Bartek.Kotkowski@CGG.com>
2021-07-22 21:57:12 -0500

    Added Windows build and run scripts

---
+++ sample/http/run.bat
@@ -0,0 +1,5 @@
+setlocal
+set PATH=%PATH%;%~dp0build\Release
+set PYTHONPATH=%~dp0build\Release
+set QT_PLUGIN_PATH=%~dp0..\..\vcpkg_installed\x64-windows\plugins
+%~dp0..\..\vcpkg_installed\x64-windows\tools\python3\python.exe
