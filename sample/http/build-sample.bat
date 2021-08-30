587eb2e2d2dda03023574e06dc0b0b7f9a4e445f
Kotkowski <Bartek.Kotkowski@CGG.com>
2021-08-30 12:25:49 -0500

    Added additional packages

--- sample/http/build-sample.bat
+++ sample/http/build-sample.bat
@@ -1,3 +1,3 @@
-%~dp0..\..\downloads\tools\cmake-3.20.2-windows\cmake-3.20.2-windows-i386\bin\cmake.exe -B build -S . -G "Visual Studio 15 2017" -A x64 -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=%~dp0..\..\scripts\buildsystems\vcpkg.cmake -DVCPKG_TARGET_TRIPLET=x64-windows -DVCPKG_INSTALLED_DIR=%~dp0..\..\vcpkg_installed%
-"%DevEnvDir%devenv.exe" build\http.sln /build Release
+%~dp0..\..\downloads\tools\cmake-3.20.2-windows\cmake-3.20.2-windows-i386\bin\cmake.exe -B build -S . -G "Visual Studio 15 2017" -A x64 -DCMAKE_TOOLCHAIN_FILE=%~dp0..\..\scripts\buildsystems\vcpkg.cmake -DVCPKG_TARGET_TRIPLET=x64-windows -DVCPKG_INSTALLED_DIR=%~dp0..\..\vcpkg_installed%
+"%DevEnvDir%devenv.exe" build\http.sln /build Debug
 
--- sample/http/run.bat
+++ sample/http/run.bat
@@ -1,5 +1,5 @@
 setlocal
-set PATH=%PATH%;%~dp0build\Release
-set PYTHONPATH=%~dp0build\Release
+set PATH=%PATH%;%~dp0build\Debug
+set PYTHONPATH=%~dp0build\Debug
 set QT_PLUGIN_PATH=%~dp0..\..\vcpkg_installed\x64-windows\plugins
 %~dp0..\..\vcpkg_installed\x64-windows\tools\python3\python.exe test.py
